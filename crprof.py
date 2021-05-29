daf4d2df25968a33f3612c1e8fe6f4e3dec96ba1
Koji Ishii <kojii@chromium.org>
2021-05-29 09:31:13 +0900

    crprof.py

--- crprof.py
+++ crprof.py
@@ -2,8 +2,8 @@
 import argparse
 import logging
 import os
-import pathlib
 import re
+import shlex
 import signal
 import subprocess
 import sys
@@ -29,9 +29,52 @@ class Profiler(object):
         self.perf.wait()
         logger.info('perf "%s" done.', self.perf_data_path)
 
-    def pprof(self):
-        args = ['pprof', '-svg', self.perf_data_path]
+    def pprof(self, pprof_options):
+        args = ['pprof']
+        if pprof_options is None:
+            args.append('-svg')
+        else:
+            args += shlex.split(pprof_options)
+        args.append(self.perf_data_path)
+        logger.info('Running %s', args)
         subprocess.run(args)
+
+    @staticmethod
+    def interactive(profilers, options):
+        while True:
+            if options.keep:
+                print('[d] Delete perf data when done')
+            else:
+                print('[k] Keep perf data when done')
+            print(f'[-*] Set pprof options (current: "{options.pprof}")')
+            for i, profiler in enumerate(profilers):
+                print(f'[{i + 1}] {profiler.perf_data_path} '
+                      f'{os.stat(profiler.perf_data_path).st_size:10,}')
+            print(f'Run pprof for: ', end='', flush=True)
+            line = sys.stdin.readline().rstrip()
+            if not line:
+                break
+            if line == 'k':
+                options.keep = True
+                continue
+            elif line == 'd':
+                options.keep = False
+                continue
+            elif line[0] == '-':
+                options.pprof = line
+                continue
+            try:
+                i = int(line) - 1
+                profiler = profilers[i]
+                profiler.pprof(options.pprof)
+                if not options.keep:
+                    os.unlink(profiler.perf_data_path)
+                    del profilers[i]
+            except ValueError:
+                print(f'"{line}" not recognized.')
+        if not options.keep:
+            for profiler in profilers:
+                os.unlink(profiler.perf_data_path)
 
 
 def run(args, options):
@@ -61,9 +104,12 @@ def run(args, options):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('program',
+                        nargs='?',
                         default=os.path.join(os.environ.get('OUT'), 'chrome'))
     parser.add_argument('args', nargs='*')
-    parser.add_argument('-f', '--frequency')
+    parser.add_argument('-f', '--frequency', help='perf frequency')
+    parser.add_argument('-k', '--keep', help='keep perf.data')
+    parser.add_argument('--pprof', default='-svg', help='pprof options')
     options = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
     args = [
@@ -79,6 +125,7 @@ def main():
     profilers = run(args, options)
     for profiler in profilers:
         profiler.wait()
+    Profiler.interactive(profilers, options)
 
 
 if __name__ == '__main__':
